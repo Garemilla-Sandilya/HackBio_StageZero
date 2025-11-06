71c1bce465b49621a8caacce734f6346fd6a69de
Garemilla-Sandilya <144174592+Garemilla-Sandilya@users.noreply.github.com>
2025-11-06 16:15:26 -0500

    Add files via upload

---
+++ Stage_0.py
@@ -0,0 +1,70 @@
+team_aspartic = [
+    {
+	"name": "Sri Sathya Sandilya Garemilla",
+        "slack": "Garemilla Sandilya",
+        "country": "United States of America",
+        "hobby": "Coding",
+        "affiliation": "Bioinformatics and Molecular Biochemistry",
+        "favourite_gene": "FOS",
+        "sequence": "AACCGCATCTGCAGCGAGCATCTGAGAAGCCAAGACTGAGCCGGCGGCCGCGGCGCAGCGAACGAGCAGT"
+     },
+    {
+        "name": "Akinjide",
+        "slack": "Akinjide Akinowose",
+        "country": "Nigeria",
+        "hobby": "Coding",
+        "affiliation": "Biomedical Informatics",
+        "favourite_gene": "PTEN",
+        "sequence": "ATGACAGCCATCATCAAAGAG"
+    },
+    {
+        "name": "Fakhia Mubashir",
+        "slack": "Fakhia Mubashir",
+        "country": "Pakistan",
+        "hobby": "Reading",
+        "affiliation": "None",
+        "favourite_gene": "TRPV6",
+        "sequence": "ATCGTCGCCCGATACGATCCAGTA"
+    },
+    {
+        "name": "Funmilayo",
+        "slack": "Funmilayo Christianah Ligali",
+        "country": "Nigeria",
+        "hobby": "Cooking",
+        "affiliation": "Biochemistry",
+        "favourite_gene": "TP53",
+        "sequence": "ATGGAGGAGCCGCAGTCAGA"
+    },
+    {
+        "name": "Niraj Pun Magar",
+        "slack": "Niraj Pun Magar",
+        "country": "Nepal",
+        "hobby": "UX design",
+        "affiliation": "Bioinformatics",
+        "favourite_gene": "APOE",
+        "sequence": "ATGCATGCGCGCATGC"
+    },
+    {
+        "name": "Himanshu Pundir",
+        "slack": "HIMANSHU PUNDIR",
+        "country": "India",
+        "hobby": "Editing",
+        "affiliation": "Bioinformatics",
+        "favourite_gene": "SIR1",
+        "sequence": "ATGTCTATAAAAGGAAAT"
+    },
+    {
+        "name": "Diksha Shetty",
+        "slack": "Diksha Shetty",
+        "country": "India",
+        "hobby": "Reading",
+        "affiliation": "Bioinformatics",
+        "favourite_gene": "MYC",
+        "sequence": "GGAGTTTATTCATAACGCGCTCTCCAAGTATACGTGGCAATGCGTT"
+    }
+]
+
+print(team_aspartic)
+
+
+
